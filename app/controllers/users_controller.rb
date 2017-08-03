6916114da926925123d10faddeb0f5b400d59d2c
Jean <jean-marco-10@hotmail.com>
2017-08-02 18:59:02 -0600

    Dont know what did I commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,11 +19,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       flash[:success] = "Your account was succesfully updated"
       redirect_to articles_path
@@ -33,13 +31,17 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user= User.find(params[:id])
+  
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 5)
   end
   
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
